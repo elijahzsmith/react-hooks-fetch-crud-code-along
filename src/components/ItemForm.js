832868747312a061d1a7e180522e9965fb2ae88d
Elijah Smith <elijahsmith.m@gmail.com>
2022-04-10 12:17:12 -0500

    CRUD in React

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,11 +1,33 @@
 import React, { useState } from "react";
+import { v4 as uuid } from 'uuid'
 
-function ItemForm() {
+function ItemForm({ items, setItems, onAddItem }) {
   const [name, setName] = useState("");
   const [category, setCategory] = useState("Produce");
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const newItem = {
+      //id: uuid(),
+      name: name,
+      category: category,
+      isInCart: false
+    }
+    console.log(newItem)
+    fetch(`http://localhost:4000/items`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        //Accept: 'application/json',
+      },
+      body: JSON.stringify(newItem)
+    })
+    .then(res => res.json())
+    .then(newItem => onAddItem(newItem))
+  }
+  
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={handleSubmit}>
       <label>
         Name:
         <input
